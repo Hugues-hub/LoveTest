dadad96cc61432d7c24ca29f46854df6c8f2cf58
Hugues <52865705+KodoK-Hugues@users.noreply.github.com>
2020-09-06 17:35:16 +0200

    style="width:100%"

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
                 <div class="card-header">Workshop Laravel</div>
 
                 <div class="card-body" style="text-align:center";>
-                  <img src="img/LoveSon.jpg" alt="loveSon" style="width:60vw";>
+                  <img src="img/LoveSon.jpg" alt="loveSon" style="width:100%";>
             </div>
         </div>
     </div>
